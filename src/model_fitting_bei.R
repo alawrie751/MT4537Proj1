47c5a82883c9384a9e0ad901601c35745cd16ea3
alawrie751 <66668839+alawrie751@users.noreply.github.com>
2022-03-15 15:49:47 +0000

    lots of stuff

--- src/model_fitting_bei.R
+++ src/model_fitting_bei.R
@@ -14,6 +14,9 @@ pdf(file = "results/initialintenseplots/beidat.pdf")
 plot(bei, main = "bei data")
 dev.off()
 
+# set the seed so simulations are reproducible
+set.seed(150)
+
 # Fit Kernel Smooth ------
 
 # Fit kernel smooth as initial estimate of intensity
@@ -66,6 +69,17 @@ pdf(file = "results/ppmmodels/best_xy.pdf")
 plot(predict(mod_poly6_xy), main = "Model Fitted with Polynomial of Degree 6")
 dev.off()
 
+datPCF_x <- pcfinhom(dat, mod_poly6_xy, correction = "translate")
+datPCF_sim_x <- pcfinhom(simulate(mod_poly6_xy, nsim = 1)[[1]], mod_poly6_xy, correction = "translate")
+
+pdf(file = "results/ppmmodels/pcf_realxy.pdf")
+plot(datPCF_x, main = "Real Data")
+dev.off()
+
+pdf(file = "results/ppmmodels/pcf_simxy.pdf")
+plot(datPCF_sim_x, main = "Simulated Data")
+dev.off()
+
 # Fit Models Using ppm for Elevation and Gradient ------
 
 mod_elev <- ppm(dat ~ elev, data = dat_extra)
@@ -77,11 +91,75 @@ mod_g_e <- ppm(dat ~ elev + grad, data = dat_extra)
 mod_ge <- ppm(dat ~ elev * grad, data = dat_extra)
 
 pdf(file = "results/ppmmodels/best_ge.pdf")
-plot(predict(mod_ge), main = "Model Fitted with the Interaction of these Terms")
+plot(predict(mod_ge), main = "Model Fitted with the Interaction Term")
 dev.off()
-
 
 datPCF <- pcfinhom(dat, mod_ge, correction = "translate")
 datPCF_sim <- pcfinhom(simulate(mod_ge, nsim = 1)[[1]], mod_ge, correction = "translate")
+
+pdf(file = "results/ppmmodels/pcf_real.pdf")
+plot(datPCF, main = "Real Data")
+dev.off()
+
+pdf(file = "results/ppmmodels/pcf_sim.pdf")
+plot(datPCF_sim, main = "Simulated Data")
+dev.off()
+
+# Fit Thomas Models ------
+
+mod_tom <- kppm(dat ~ poly(x, 6) + poly(y, 6), clusters = "Thomas", method = "palm")
+
+pdf(file = "results/ppmmodels/best_tom.pdf")
+plot(predict(mod_tom), main = "Thomas Cluster Model Fitted with Polynomial of Degree 6")
+dev.off()
+
+pdf(file = "results/ppmmodels/best_tom_dat.pdf")
+plot(mod_tom, main = "Thomas Cluster Model Fitted with Polynomial of Degree 6 with Data")
+dev.off()
+
+datPCF_tom <- pcfinhom(dat, mod_tom, correction = "translate")
+datPCF_sim_tom <- pcfinhom(simulate(mod_tom, nsim = 1)[[1]], mod_tom, correction = "translate")
+
+pdf(file = "results/ppmmodels/pcf_realtom.pdf")
+plot(datPCF_tom, main = "Real Data")
+dev.off()
+
+pdf(file = "results/ppmmodels/pcf_simtom.pdf")
+plot(datPCF_sim_tom, main = "Simulated Data")
+dev.off()
+
+# Simulations from the Cox Model ------
+
+# Set a new seed for these simulations
+set.seed(500)
+
+# Simulate from the Thomas model fitted above
+tomsim <- simulate(mod_tom, nsim = 2)
+
+pdf(file = "results/ppmmodels/tomsim.pdf")
+plot(tomsim, main = "Data Simulated from the Thomas Model")
+dev.off()
+
+# Simulations from the Poisson Model ------
+
+set.seed(600)
+
+poly6_sim <- simulate(mod_poly6_xy, nsim = 2)
+
+pdf(file = "results/ppmmodels/poly6sim.pdf")
+plot(poly6_sim, main = "Data Simulated from the Poisson Process Model")
+dev.off()
+
+
+
+
+
+
+
+
+
+
+
+
 
 
